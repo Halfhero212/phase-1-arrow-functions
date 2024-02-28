9b5e7f4673fd8d3687a0cd686de99660e50591ba
Nooralhussein <lighthero212@gmail.com>
2024-02-28 23:07:13 +0300

    Done

---
+++ index.js
@@ -0,0 +1,17 @@
+const foo = function() {
+    return 'bar';
+  };
+  
+  const add = (parameter1, parameter2) => parameter1 + parameter2;
+  
+  const twoAdder = x => x + 2;
+  
+  const sum = (parameter1, parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 + parameter2;
+  };
+  
+  const nums = [1, 2, 3];
+  const squares = nums.map(x => x ** 2);
+  
